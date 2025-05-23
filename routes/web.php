2b516c5308309596e07b5f40423828c380cfb9c9
devbickoel <161627726+devbickoel@users.noreply.github.com>
2025-05-24 03:08:31 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,6 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Middleware\AllowTelegramIframe;
 
-Route::get('/', function () {
-    return view('welcome');
-})->middleware(AllowTelegramIframe::class);
+Route::middleware([AllowTelegramIframe::class])->group(function () {
+    Route::view('/welcome', 'welcome'); // contoh
+});
